25407c08f94517424bab3800ee346fbbebf533b7
JavierBoyaca <68155136+bamg359@users.noreply.github.com>
2024-11-28 15:17:45 -0500

    codigo3 validar formulario se crea proyecto

---
+++ src/main/java/com/example/Servidorsura6/modelos/Vehiculo.java
@@ -0,0 +1,14 @@
+package com.example.Servidorsura6.modelos;
+
+public class Vehiculo {
+
+
+    //ID(AUTO LONG)
+    //PLACA (STRING)
+    //MODELO (STRING)
+    //MARCA (STRING)
+    //CILINDRAJE (STRING)
+    //COLOR (STRING)
+    //DESCRIPCION (STRING)
+    //NUMEROSINIESTROS (INT)(Integer)
+}
