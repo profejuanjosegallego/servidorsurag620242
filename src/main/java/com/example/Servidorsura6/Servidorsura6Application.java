b0f7703eebb88d533d88a5dde27a25b8ac988127
JavierBoyaca <68155136+bamg359@users.noreply.github.com>
2024-12-12 15:01:45 -0500

    ok

--- src/main/java/com/example/Servidorsura6/Servidorsura6Application.java
+++ src/main/java/com/example/Servidorsura6/Servidorsura6Application.java
@@ -2,12 +2,28 @@ package com.example.Servidorsura6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Servidorsura6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Servidorsura6Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")  // Permite todas las rutas
+						.allowedOrigins("*")  // Permite cualquier origen
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+						.allowedHeaders("*");  // Permite todos los encabezados
+			}
+		};
 	}
 
 }
